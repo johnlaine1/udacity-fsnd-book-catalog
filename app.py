0d7fe919fd35439f14999b9c753e26289d748589
John Laine <john.laine5896@gmail.com>
2016-10-06 23:14:20 +0000

    Issue #3: Set up basic templating

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask import session as login_session
 import config
 
 
@@ -9,52 +10,53 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/books')
 def showBooks():
-    return 'route: / and /books ---- parameters: none'
+    return render_template('front.html')
 
 
 # CATEGORY ROUTES
 @app.route('/book/category/add')
 def addBookCategory():
-    return 'route: /book/category/add ---- parameters: none'
+    return render_template('addBookCategory.html')
 
 
 @app.route('/book/category/<int:book_cat_id>')
 def showBookCategory(book_cat_id):
-    return 'route: /book/category/<int:book_cat_add> ---- paramenters: {}'.format(book_cat_id)
+    return render_template('showBookCategory.html')
 
 
 @app.route('/book/category/<int:book_cat_id>/edit')
 def editBookCategory(book_cat_id):
-    return 'route: /book/category/<int:book_cat_add>/edit ---- paramenters: {}'.format(book_cat_id)
+    return render_template('editBookCategory.html')
 
 
 @app.route('/book/category/<int:book_cat_id>/delete')
 def deleteBookCategory(book_cat_id):
-    return 'route: /book/category/<int:book_cat_add>/delete ---- paramenters: {}'.format(book_cat_id)
+    return render_template('deleteBookCategory.html')
 
 
 # BOOK ROUTES
 @app.route('/book/add')
 def addBook():
-    return 'route: /book/add ---- parameters: none'
+    return render_template('addBook.html')
 
 
 @app.route('/book/<int:book_id>')
 def showBook(book_id):
-    return 'route: /book/<int:book_id> ---- parameters: {}'.format(book_id)
+    return render_template('showBook.html')
 
 
 @app.route('/book/<int:book_id>/edit')
 def editBook(book_id):
-    return 'route: /book/<int:book_id>/edit ---- parameters: {}'.format(book_id)
+    return render_template('editBook.html')
 
 
 @app.route('/book/<int:book_id>/delete')
 def deleteBook(book_id):
-    return 'route: /book/<int:book_id>/delete ---- parameters: {}'.format(book_id)
+    return render_template('deleteBook.html')
 
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key = config.secret_key
     app.run(config.host, config.port)
 
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 host = '0.0.0.0'
 port = 8080
+secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
